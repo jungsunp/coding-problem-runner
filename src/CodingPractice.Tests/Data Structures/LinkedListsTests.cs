7df4ebddc0687828b20fc11c3766fc624180a51b
Jung Park <jungsun.park@relativity.com>
2024-06-23 14:57:43 -0500

    create linked List class and first problem

---
+++ src/CodingPractice.Tests/Data Structures/LinkedListsTests.cs
@@ -0,0 +1,31 @@
+﻿namespace CodingPractice.Tests;
+using Xunit;
+using Xunit.Abstractions;
+using CodingPractice;
+
+public class LinkedListsTests
+{
+	[Theory]
+	[InlineData(new int[] {}, new int[] {})] // Empty list
+	[InlineData(new int[] {1}, new int[] {1})] // Single node
+	[InlineData(new int[] {1, 2, 3, 2, 4}, new int[] {1, 2, 3, 4})] // Five nodes with duplicates
+	[InlineData(new int[] {1, 2, 3, 4, 5, 3, 2, 6, 7, 5}, new int[] {1, 2, 3, 4, 5, 6, 7})] // Ten nodes with duplicates
+	public void RemoveDups_VariousLists_RemovesDuplicatesAsExpected(int[] input, int[] expected)
+	{
+		var list = new LinkedList<int>(input);
+		LinkedLists.RemoveDups(list);
+		Assert.Equal(expected, list);
+	}
+
+	[Theory]
+	[InlineData(new int[] {}, new int[] {})] // Empty list
+	[InlineData(new int[] {1}, new int[] {1})] // Single node
+	[InlineData(new int[] {1, 2, 3, 2, 4}, new int[] {1, 2, 3, 4})] // Five nodes with duplicates
+	[InlineData(new int[] {1, 2, 3, 4, 5, 3, 2, 6, 7, 5}, new int[] {1, 2, 3, 4, 5, 6, 7})] // Ten nodes with duplicates
+	public void RemoveDupsNoBuffer_VariousLists_RemovesDuplicatesAsExpected(int[] input, int[] expected)
+	{
+		var list = new LinkedList<int>(input);
+		LinkedLists.RemoveDupsNoBuffer(list);
+		Assert.Equal(expected, list);
+	}
+}
---
+++ src/CodingPractice/Data Structures/LinkedLists.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingPractice
+{
+	public static class LinkedLists
+	{
+		// #2.1
+		// Time: O(n)
+		// Space: O(n)
+		public static void RemoveDups(LinkedList<int> list) {
+
+			if (list.Count < 2)
+			{
+				return;
+			}
+
+			var hashSet = new HashSet<int> { list.First.Value };
+			var node = list.First.Next;
+
+			while (node != null)
+			{
+				var nextNode = node.Next;
+
+				if (!hashSet.Contains(node.Value))
+				{
+					hashSet.Add(node.Value); // keep track of values in hash set
+				}
+				else
+				{
+					list.Remove(node);
+				}
+
+				node = nextNode;
+			}
+		}
+
+		// #2.1
+		// Time: O(n^2)
+		// Space: O(1)
+		public static void RemoveDupsNoBuffer(LinkedList<int> list)
+		{
+			if (list.Count < 2)
+			{
+				return;
+			}
+
+			var nodeOne = list.First;
+			var nodeTwo = list.First.Next;
+
+			while (nodeOne != null && nodeTwo != null)
+			{
+				while (nodeTwo != null)
+				{
+					if (nodeOne.Value == nodeTwo.Value) {
+						var node = nodeTwo.Next;
+						list.Remove(nodeTwo);
+						nodeTwo = node;
+					} else {
+						nodeTwo = nodeTwo.Next;
+					}
+				}
+
+				nodeOne = nodeOne.Next;
+				nodeTwo = nodeOne.Next;
+			}
+		}
+	}
+}
