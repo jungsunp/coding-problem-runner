04864776ff87d3848146fb307ba2f10b2ca93cb3
Jung Park <jungsun.park@relativity.com>
2024-06-29 10:11:59 -0500

    min stack implementation

---
+++ src/CodingPractice.Tests/Data Structures/StacksAndQueuesTests.cs
@@ -0,0 +1,57 @@
+﻿namespace CodingPractice.Tests;
+using Xunit;
+using CodingPractice;
+
+public class StacksAndQueuesTests
+{
+	[Fact]
+	public void StackMin_PushItems_MinValueUpdatesCorrectly()
+	{
+		var stack = new StackMin();
+		stack.Push(5);
+		Assert.Equal(5, stack.Min());
+		stack.Push(6);
+		Assert.Equal(5, stack.Min());
+		stack.Push(3);
+		Assert.Equal(3, stack.Min());
+		stack.Push(7);
+		Assert.Equal(3, stack.Min());
+	}
+
+	[Fact]
+	public void StackMin_PopItems_MinValueUpdatesCorrectly()
+	{
+		var stack = new StackMin();
+		stack.Push(5);
+		stack.Push(2);
+		stack.Push(4);
+		stack.Push(1);
+		stack.Push(3);
+
+		stack.Pop();
+		Assert.Equal(1, stack.Min());
+		stack.Pop();
+		Assert.Equal(2, stack.Min());
+		stack.Pop();
+		Assert.Equal(2, stack.Min());
+	}
+
+	[Fact]
+	public void StackMin_NonEmptyStack_ReturnsCorrectMin()
+	{
+		var stack = new StackMin();
+		stack.Push(2);
+		stack.Push(1);
+		stack.Push(3);
+
+		Assert.Equal(1, stack.Min());
+	}
+
+	[Fact]
+	public void StackMin_EmptyStack_ThrowsException()
+	{
+		var stack = new StackMin();
+
+		Assert.Throws<InvalidOperationException>(() => stack.Min());
+	}
+}
---
+++ src/CodingPractice/Data Structures/StacksAndQueues.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace CodingPractice
+{
+	public class StacksAndQueues
+	{
+
+	}
+
+
+	// #3.2
+	// Time: O(1) for all push, pop, min
+	public class StackMin
+	{
+		private class StackMinNode
+		{
+			public int Value;
+			public StackMinNode Next;
+			public StackMinNode SubStackMinNode;
+
+			public StackMinNode(int value)
+			{
+				this.Value = value;
+			}
+		}
+
+		private StackMinNode top;
+
+		private StackMinNode minNode;
+
+		public void Push(int value)
+		{
+			var node = new StackMinNode(value);
+
+			if (top != null)
+			{
+				node.Next = top;
+			}
+
+			if (minNode != null)
+			{
+				if (minNode.Value > node.Value)
+				{
+					minNode = node;
+				}
+			}
+			else
+			{
+				minNode = node;
+			}
+
+			node.SubStackMinNode = minNode; // each node stores min of substack
+
+			top = node;
+		}
+
+		public int Pop()
+		{
+			if (top == null)
+			{
+				throw new InvalidOperationException("Empty Stack");
+			}
+
+			if (top.Value == this.minNode.Value)
+			{
+				this.minNode = top.Next.SubStackMinNode;
+			}
+
+			var ret = top;
+			top = top.Next;
+			return ret.Value;
+		}
+
+		public int Min()
+		{
+			if (minNode == null)
+			{
+				throw new InvalidOperationException("Stack is empty");
+			}
+			return this.minNode.Value;
+		}
+	}
+}
