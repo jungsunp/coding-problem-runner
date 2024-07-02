b81a4f4dfd9e08f32da0cde2f9fe23a41dc8b187
Jung Park <jungsun.park@relativity.com>
2024-07-02 09:12:10 -0500

    Route between nodes

---
+++ src/CodingPractice.Tests/Data Structures/TreesAndGraphsTests.cs
@@ -0,0 +1,92 @@
+﻿namespace CodingPractice.Tests;
+using CodingPractice;
+using Xunit;
+
+public class TreesAndGraphsTests
+{
+	[Fact]
+	public void RouteExists_DirectRoute_ReturnsTrue()
+	{
+		// Arrange
+		var graph = new Graph(); // Assuming you have a Graph class
+		var nodeA = new Node("A");
+		var nodeB = new Node("B");
+		graph.AddNode(nodeA);
+		graph.AddNode(nodeB);
+		graph.AddEdge(nodeA, nodeB);
+
+		// Act
+		bool result = TreesAndGraphs.RouteBetweenNodes(nodeA, nodeB);
+
+		// Assert
+		Assert.True(result);
+	}
+
+	[Fact]
+	public void RouteDoesNotExist_ReturnsFalse()
+	{
+		// Arrange
+		var graph = new Graph();
+		var nodeA = new Node("A");
+		var nodeC = new Node("C");
+		graph.AddNode(nodeA);
+		graph.AddNode(nodeC);
+		// No edge added between A and C
+
+		// Act
+		bool result = TreesAndGraphs.RouteBetweenNodes(nodeA, nodeC);
+
+		// Assert
+		Assert.False(result);
+	}
+
+	   [Fact]
+	public void RouteExists_IndirectRoute_ReturnsTrue()
+	{
+		// Arrange
+		var graph = new Graph();
+		var nodeA = new Node("A");
+		var nodeB = new Node("B");
+		var nodeC = new Node("C");
+		graph.AddNode(nodeA);
+		graph.AddNode(nodeB);
+		graph.AddNode(nodeC);
+		graph.AddEdge(nodeA, nodeB);
+		graph.AddEdge(nodeB, nodeC);
+
+		// Act
+		bool result = TreesAndGraphs.RouteBetweenNodes(nodeA, nodeC);
+
+		// Assert
+		Assert.True(result);
+	}
+
+	[Fact]
+	public void EmptyGraph_ReturnsFalse()
+	{
+		// Arrange
+		var graph = new Graph();
+		var nodeA = new Node("A");
+		var nodeC = new Node("C");
+
+		// Act
+		bool result = TreesAndGraphs.RouteBetweenNodes(nodeA, nodeC);
+
+		// Assert
+		Assert.False(result);
+	}
+
+	[Fact]
+	public void NodesAreNull_ReturnsFalse()
+	{
+		// Arrange
+		Node nodeA = null;
+		Node nodeC = null;
+
+		// Act
+		bool result = TreesAndGraphs.RouteBetweenNodes(nodeA, nodeC);
+
+		// Assert
+		Assert.False(result);
+	}
+}
---
+++ src/CodingPractice/Data Structures/TreesAndGraphs.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingPractice
+{
+	public static class TreesAndGraphs
+	{
+		// #4.1
+		public static bool RouteBetweenNodes(Node n1, Node n2)
+		{
+			if (n1 == null || n2 == null)
+			{
+				return false;
+			}
+
+			if (n1 == n2)
+			{
+				return true;
+			}
+
+			Queue<Node> queue = new Queue<Node>();
+			n1.Visited = true;
+			queue.Enqueue(n1);
+
+			while (queue.Count > 0)
+			{
+				var node = queue.Dequeue();
+				foreach (var neighbor in node.Adjacent)
+				{
+					if (neighbor == n2)
+					{
+						return true;
+					}
+
+					if (!neighbor.Visited)
+					{
+						neighbor.Visited = true;
+						queue.Enqueue(neighbor);
+					}
+				}
+			}
+
+			return false;
+		}
+
+	}
+
+
+	public class Node
+	{
+		public object Value { get; set; }
+		public  List<Node> Adjacent { get; set; }
+		public bool Visited = false;
+
+		public Node(object value)
+		{
+			this.Value = value;
+			Adjacent = new List<Node>();
+		}
+
+		 public void AddAdjacent(Node node)
+		{
+			Adjacent.Add(node);
+		}
+	}
+
+
+	public class Graph
+	{
+		public List<Node> Nodes { get; private set; }
+
+		public Graph()
+		{
+			Nodes = new List<Node>();
+		}
+
+		public void AddNode(Node node)
+		{
+			Nodes.Add(node);
+		}
+
+		public void AddEdge(Node from, Node to)
+		{
+			from.AddAdjacent(to);
+		}
+	}
+}
