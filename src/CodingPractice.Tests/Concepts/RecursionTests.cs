a7b7dd3558087a2e9964264e016d4dd7a8ef8b38
Jung Park <jungsun.park@relativity.com>
2024-07-12 08:54:42 -0500

    magic index

--- src/CodingPractice.Tests/Concepts/RecursionTests.cs
+++ src/CodingPractice.Tests/Concepts/RecursionTests.cs
@@ -10,5 +10,61 @@ namespace CodingPractice.Tests.Concepts
 {
 	public class RecursionTests
 	{
+
+		[Theory]
+		[InlineData(0, 0)]
+		[InlineData(1, 1)]
+		[InlineData(2, 2)]
+		[InlineData(3, 4)]
+		[InlineData(4, 7)]
+		[InlineData(5, 13)]
+		[InlineData(10, 274)] // These values assume a specific implementation of TripleStep
+		[InlineData(11, 504)]
+		public void TripleStep_ValidSteps_ReturnsExpectedResult(int steps, int expected)
+		{
+			// Act
+			int result = Recursion.TripleStep(steps);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void TripleStep_IntegerOverflow_ThrowsOverflowException()
+		{
+			// Arrange
+			int stepsThatCauseOverflow = 37;
+
+			// Act & Assert
+			Assert.Throws<OverflowException>(() => Recursion.TripleStep(stepsThatCauseOverflow));
+		}
+
+		[Fact]
+		public void MagicIndex_EmptyArray_ReturnsNull()
+		{
+			// Arrange
+			int[] arr = new int[0];
+
+			// Act
+			var result = Recursion.MagicIndex(arr);
+
+			// Assert
+			Assert.Null(result);
+		}
+
+		[Theory]
+		[InlineData(new int[] {0}, 0)] // Array with 1 element that is a magic index
+		[InlineData(new int[] {-1, 0, 0, 3, 5, 6, 7, 8, 9}, 3)] // Array with 9 elements, magic index at position 3
+		[InlineData(new int[] {0, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 0)] // Array with 10 elements, magic index at position 0
+		[InlineData(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, null)] // Array with 10 elements, no magic index
+		[InlineData(new int[] {-10, -5, 2, 2, 2, 3, 4, 8, 9, 12, 13}, 2)] // Array with duplicates, magic index at position 2
+		public void MagicIndex_ValidCases_ReturnsExpectedResult(int[] arr, int? expected)
+		{
+			// Act
+			var result = Recursion.MagicIndex(arr);
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
 	}
 }
--- src/CodingPractice/Concepts/Recursion.cs
+++ src/CodingPractice/Concepts/Recursion.cs
@@ -16,10 +16,15 @@ namespace CodingPractice.Concepts
 
 		private static int TripleStep(int n, int[] memo)
 		{
-			if (n == 0) { return 0;}
-			if (n == 1) { return 1;}
-			if (n == 2) { return 2;}
-			if (n == 3) { return 4;}
+			if (n <= 0) { return 0; }
+			if (n == 1) { return 1; }
+			if (n == 2) { return 2; }
+			if (n == 3) { return 4; }
+
+			if (n > 36)
+			{
+				throw new OverflowException();
+			}
 
 			if (memo[n] == 0)
 			{
@@ -27,6 +32,58 @@ namespace CodingPractice.Concepts
 			}
 
 			return memo[n];
+		}
+
+		// #8.3
+		// Time: O(log n)
+		// Space: O(log n)
+		// Assume: arr is sorted (ints may not be unique)
+		public static int? MagicIndex(int[] arr)
+		{
+			if (arr.Length < 1)
+			{
+				return null;
+			}
+
+			return MagicIndex(arr, 0, arr.Length - 1);
+		}
+
+
+		private static int? MagicIndex(int[] arr, int start, int end)
+		{
+			int mid = (start + end) / 2;
+
+			if (start > end)
+			{
+				return null; // magic index not found
+			}
+
+			if (arr[mid] == mid)
+			{
+				return mid; // found magic index
+			}
+
+			if (arr[mid] > mid)
+			{
+				int? leftHalfMagicIndex = MagicIndex(arr, start, mid -1);
+				if (leftHalfMagicIndex != null)
+				{
+					return leftHalfMagicIndex;
+				}
+
+				return MagicIndex(arr, arr[mid], end);
+			}
+			else
+			{
+				int? rightHalfMagicIndex = MagicIndex(arr, mid + 1, end);
+				if (rightHalfMagicIndex != null)
+				{
+					return rightHalfMagicIndex;
+				}
+
+				return MagicIndex(arr, start, arr[mid]);
+			}
+
 		}
 	}
 }
