665f5812c833581b3e7fe7c5c0d958d94cc54b79
Jung Park <jungsun.park@relativity.com>
2024-08-19 08:36:50 -0500

    new leet code problem additions - array, string, two pointers

---
+++ src/CodingPractice/Leetcode/ArrayString.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Text;
+
+namespace CodingPractice.Leetcode
+{
+	public class ArrayString {
+
+		// Time: O(m + n)
+		// Space: O(m + n)
+		public string MergeAlternately(string word1, string word2) {
+			int minLength = Math.Min(word1.Length, word2.Length);
+			var builder = new StringBuilder();
+
+			for (int i = 0; i < minLength; i++)
+			{
+				builder.Append(word1[i]);
+				builder.Append(word2[i]);
+			}
+
+			if (minLength < word1.Length)
+			{
+				builder.Append(word1.Substring(minLength));
+			}
+			else if (minLength < word2.Length)
+			{
+				builder.Append(word2.Substring(minLength));
+			}
+
+			return builder.ToString();
+		}
+
+
+		// Time: O(m + n)
+		// Space: O(1)
+		public string GcdOfStrings(string str1, string str2) {
+			if (str1 + str2 != str2 + str1) {
+				return "";
+			}
+
+			int gcd = Gcd(str1.Length, str2.Length);
+
+			return gcd > 0 ? str1.Substring(0, gcd) : "";
+		}
+
+		public int Gcd(int j, int k) {
+			int min = Math.Min(j, k);
+
+			for (int i = min; i > 0; i--) {
+				if (j % i == 0 && k % i == 0) {
+					return i;
+				}
+			}
+
+			return -1; // gcd not found;
+		}
+
+		// Time: O(n)
+		// Space: O(n)
+		public string ReverseWords(string s) {
+			StringBuilder retBuilder = new StringBuilder();
+			StringBuilder wordBuilder = new StringBuilder();
+
+			for (int i = s.Length - 1; i >= 0; i--) {
+				if (s[i] != ' ') {
+					wordBuilder.Insert(0, s[i]);
+				} else {
+					if (wordBuilder.Length != 0) {
+						string word = wordBuilder.ToString();
+						if (retBuilder.Length > 0) {
+							retBuilder.Append($" {word}");
+						} else {
+							retBuilder.Append(word);
+						}
+						wordBuilder.Clear();
+					}
+				}
+			}
+
+			if (wordBuilder.Length == 0) {
+				return retBuilder.ToString();
+			} else {
+				return retBuilder.Length > 0 ?  $"{retBuilder} {wordBuilder}" : wordBuilder.ToString();
+			}
+		}
+	}
+}
---
+++ src/CodingPractice/Leetcode/TwoPointers.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace CodingPractice.Leetcode {
+	public class TwoPointers {
+
+		// Time: O(n)
+		// Space: O(1)
+		public void MoveZeroes(int[] nums) {
+			if (nums.Length < 2) { return; }
+
+			int j = 0;
+			int k = 1;
+
+			while (k < nums.Length)
+			{
+
+				if (nums[j] == 0 && nums[k] == 0)
+				{
+					k++;
+				}
+				else if (nums[j] != 0 && nums[k] != 0)
+				{
+					j += 2;
+					k += 2;
+				}
+				else
+				{
+					// either one is non-zero
+					if (nums[j] == 0)
+					{
+						nums[j] = nums[k];
+						nums[k] = 0;
+					}
+					j++;
+					k++;
+				}
+			}
+		}
+
+		  // Time: O(n)
+		// Space: O(1)
+		public int MaxArea(int[] height) {
+			int left = 0;
+			int right = height.Length - 1;
+			int max = 0;
+
+			while (left < right)
+			{
+				int area = Math.Min(height[left], height[right]) * (right - left);
+				if (max < area)
+				{
+					max = area;
+				}
+
+				if (height[left] < height[right])
+				{
+					left++;
+				}
+				else
+				{
+					right--;
+				}
+			}
+
+			return max;
+		}
+	}
+}
