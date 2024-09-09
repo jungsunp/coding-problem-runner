0d8ede35fa9db561ae142966ab8fef160ad9855c
Jung Park <jungsun.park@relativity.com>
2024-09-09 07:59:44 -0500

    linked list

---
+++ src/CodingPractice/Leetcode/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPractice.Leetcode
+{
+	public class LinkedList
+	{
+		public class ListNode {
+			public int val;
+			public ListNode next;
+			public ListNode(int val=0, ListNode next=null) {
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		// #206. Reverse Linked List
+		// Time: O(n)
+		// Space: O(n)
+		public ListNode ReverseList(ListNode head) {
+			if (head == null || head.next == null)
+			{
+				return head;
+			}
+
+			var res = ReverseList(head.next);
+			head.next.next = head;
+			head.next = null;
+			return res;
+		}
+	}
+}
