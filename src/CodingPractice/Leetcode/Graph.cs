c70b1c25fca37e6ae45aec0db9639461cb231ea5
Jung Park <jungsun.park@relativity.com>
2024-09-19 08:02:37 -0500

    graph dfs q

--- src/CodingPractice/Leetcode/Graph.cs
+++ src/CodingPractice/Leetcode/Graph.cs
@@ -16,7 +16,7 @@ namespace CodingPractice.Leetcode
 			roomsToVisit.Enqueue(0);
 			visitedRooms.Add(0);
 
-			while (roomsToVisit.Count > 0)
+			while (roomsToVisit.Count > 0) // run BFS
 			{
 				int room = roomsToVisit.Dequeue();
 				foreach(int key in rooms[room])
@@ -30,6 +30,43 @@ namespace CodingPractice.Leetcode
 			}
 
 			return visitedRooms.Count == rooms.Count;
+		}
+
+		// #547. Number of Provinces
+		// Time: O(n^2)
+		// Space: O(n)
+		public int FindCircleNum(int[][] isConnected) {
+			int numCities = isConnected.Length;
+			HashSet<int> visited = new HashSet<int>();
+			int numProvinces = 0;
+
+			for (int i = 0; i < numCities; i++)
+			{
+				if (visited.Contains(i))
+				{
+					continue; // already visited. move over to next city
+				}
+
+				numProvinces++;
+				Stack<int> toVisit = new Stack<int>();
+				toVisit.Push(i);
+
+				while (toVisit.Count > 0) // Run DFS
+				{
+					int currentCity = toVisit.Pop();
+					visited.Add(currentCity);
+					int[] connectedCities = isConnected[currentCity];
+					for (int j = 0; j < numCities; j++)  // only need to check upper half of matrix
+					{
+						if (connectedCities[j] == 1 && !visited.Contains(j))
+						{
+							toVisit.Push(j);
+						}
+					}
+				}
+			}
+
+			return numProvinces;
 		}
 	}
 }
