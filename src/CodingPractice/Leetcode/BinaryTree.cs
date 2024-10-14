631c06226ff4a27c4ee84eef9f53a9ac46d686f7
Jung Park <jungsun.park@relativity.com>
2024-10-14 07:59:49 -0500

    postfix binary prob

--- src/CodingPractice/Leetcode/BinaryTree.cs
+++ src/CodingPractice/Leetcode/BinaryTree.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace CodingPractice.Leetcode
 {
@@ -85,6 +86,73 @@ namespace CodingPractice.Leetcode
 			if (root.val == val) { return root; }
 			else if (root.val > val) { return SearchBST(root.left, val); }
 			else { return SearchBST(root.right, val); }
+		}
+
+		// #1597. Build Binary Expression Tree From Infix Expression
+		// Time: O(n)
+		// Space: O(n)
+		public BinaryTreeNode<char> ExpTree(string s) {
+			string postfixStr = ConvertToPostfix(s);
+
+			// Build tree using postfix string
+			Stack<BinaryTreeNode<char>> stack = new Stack<BinaryTreeNode<char>>();
+			foreach (char c in postfixStr) {
+				if (Char.IsNumber(c)) {
+					stack.Push(new BinaryTreeNode<char>(c));
+				} else { // operator
+					BinaryTreeNode<char> node = new BinaryTreeNode<char>(c);
+					node.Right = stack.Pop();
+					node.Left = stack.Pop();
+					stack.Push(node);
+				}
+			}
+
+			return stack.Peek();
+		}
+
+		private string ConvertToPostfix(string s){
+			StringBuilder postfix = new StringBuilder();
+			Stack<char> opStack = new Stack<char>();
+
+			foreach (char c in s) {
+				if (Char.IsNumber(c)) { // operands
+					postfix.Append(c);
+				}
+				else if (c == '(') {
+					opStack.Push(c);
+				}
+				else if (c == ')') {
+					while (opStack.Count > 0 && opStack.Peek() != '(') {
+						postfix.Append(opStack.Pop());
+					}
+					opStack.Pop(); // get rid of '(' from stack
+				}
+				else { // operators
+					while (opStack.Count > 0 && OperatorOrder(c) <= OperatorOrder(opStack.Peek())) {
+						postfix.Append(opStack.Pop());
+					}
+					opStack.Push(c);
+				}
+			}
+
+			while (opStack.Count > 0) {
+				postfix.Append(opStack.Pop()); // put remaining operators
+			}
+
+			return postfix.ToString();
+		}
+
+		private static int OperatorOrder(char op) {
+			switch (op) {
+				case '*':
+				case '/':
+					return 2;
+				case '+':
+				case '-':
+					return 1;
+				default:
+					return -1;
+			}
 		}
 	}
 }
