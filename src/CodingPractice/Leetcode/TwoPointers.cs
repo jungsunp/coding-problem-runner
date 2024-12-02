729a272f008654cf56bccf7f7f4d603199513b1c
Jung Park <jungsun.park@relativity.com>
2024-12-02 08:05:15 -0600

    more 2 pointer qs

--- src/CodingPractice/Leetcode/TwoPointers.cs
+++ src/CodingPractice/Leetcode/TwoPointers.cs
@@ -66,5 +66,54 @@ namespace CodingPractice.Leetcode {
 
 			return max;
 		}
+
+		// #392. Is Subsequence
+		// Time: O(n)
+		// Space: O(1)
+		public bool IsSubsequence(string s, string t) {
+			int ps = 0; // pointer s
+			int pt = 0; // pointer t
+
+			while (ps < s.Length && pt < t.Length) {
+				if (s[ps] == t[pt]) {
+					ps++;
+				}
+ 
+				pt++;
+			}
+
+			return ps == s.Length;
+		}
+
+
+		// #1679. Max Number of K-Sum Pairs
+		// Time: O(n log n)
+		// Space: O (log n)
+		// Note: there is diff solution to use hash map with O(n) time and space
+		public int MaxOperations(int[] nums, int k) {
+			Array.Sort(nums);
+
+			int left = 0;
+			int right = nums.Length - 1;
+			int count = 0;
+
+			while (left < right) {
+				int sum = nums[left] + nums[right];
+
+				if (sum < k) {
+					left++;
+				}
+				else if (sum > k) {
+					right--;
+				}
+				else {
+					count++;
+					left++;
+					right--;
+				}
+			}
+
+			return count;
+		}
 	}
 }
