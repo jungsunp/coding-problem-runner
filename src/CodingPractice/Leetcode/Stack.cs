34e4753ba91662eb82b5f955cf9a3259edd0b6bb
Jung Park <jungsun.park@relativity.com>
2024-12-12 17:44:22 -0600

    stack probs

--- src/CodingPractice/Leetcode/Stack.cs
+++ src/CodingPractice/Leetcode/Stack.cs
@@ -9,47 +9,16 @@ namespace CodingPractice.Leetcode
 		// #2390. Removing Stars From a String
 		// Time: O(n)
 		// Space: O(n)
+		// Note: not stack question?
 		public string RemoveStars(string s) {
-			int i = s.Length - 1;
-			var stack = new Stack<char>();
-
-			while (i >= 0)
-			{
-				if (s[i] == '*')
-				{
-					int starCount = 1;
-					i--;
-					while (i >= 0 && s[i] == '*')
-					{
-						starCount++;
-						i--;
-					}
-
-					while (starCount > 0)
-					{
-						if (s[i] == '*')
-						{
-							starCount++;
-						}
-						else
-						{
-							starCount--;
-						}
-						i--;
-					}
-
-				}
-				else
-				{
-					stack.Push(s[i]);
-					i--;
-				}
-			}
-
 			var builder = new StringBuilder();
-			while(stack.Count > 0)
-			{
-				builder.Append(stack.Pop());
+			for (int i = 0; i < s.Length; i++) {
+				if (s[i] == '*') {
+					builder.Remove(builder.Length - 1, 1);
+				}
+				else {
+					builder.Append(s[i]);
+				}
 			}
 
 			return builder.ToString();
@@ -71,11 +40,90 @@ namespace CodingPractice.Leetcode
 					int index = indices.Pop();
 					ans[index] = i - index;
 				}
-				
+
 				indices.Push(i);
 			}
 
 			return ans;
+		}
+
+		// #735. Asteroid Collision
+		// Time: O(n)
+		// Space: O(n)
+		public int[] AsteroidCollision(int[] asteroids) {
+			var stack = new Stack<int>();
+
+			// Simulate Collision
+			for(int i = 0; i < asteroids.Length; i++) {
+
+				if (stack.Count > 0 && stack.Peek() > 0 && asteroids[i] < 0) { // collide
+					while (stack.Count > 0 && stack.Peek() > 0 && stack.Peek() < Math.Abs(asteroids[i])) {
+						stack.Pop();
+					}
+
+					if (stack.Count == 0 || stack.Peek() < 0) {
+						stack.Push(asteroids[i]);
+					}
+					else if (stack.Peek() == Math.Abs(asteroids[i])) { // same size collision
+						stack.Pop();
+					}
+				}
+				else {
+					stack.Push(asteroids[i]);
+				}
+			}
+
+			// Move remaining asteroids in stack to array
+			var ret = new int[stack.Count];
+			for(int i = stack.Count - 1; i >= 0; i--) {
+				ret[i] = stack.Pop();
+			}
+
+			return ret;
+		}
+
+		// #394. Decode String
+		// Time: O(n)
+		// Space: O(n)
+		// Note: Check Leetcode for different view on time & space complexityies
+		public string DecodeString(string s) {
+
+			// Use stack to decode all repeats
+			var stack = new Stack<string>();
+			foreach (var ch in s) {
+				if (ch != ']') {
+					stack.Push(ch.ToString());
+				}
+				else {
+					var strBuilder = new StringBuilder();
+					while (stack.Peek()[0] != '[') {
+						strBuilder.Insert(0, stack.Pop());
+					}
+					var subStr = strBuilder.ToString();
+
+					stack.Pop(); // Remove '[' from stack
+
+					var cntBuilder = new StringBuilder();
+					while (stack.Count > 0 && Char.IsNumber(stack.Peek()[0])) {
+						cntBuilder.Insert(0, stack.Pop());
+					}
+					int cnt = Int32.Parse(cntBuilder.ToString());
+
+					for (int i = 0; i < cnt - 1; i++) {
+						strBuilder.Append(subStr);
+					}
+
+					stack.Push(strBuilder.ToString());
+				}
+			}
+
+			// Use stack to generate return string
+			var builder = new StringBuilder();
+			while (stack.Count > 0) {
+				builder.Insert(0, stack.Pop());
+			}
+
+			return builder.ToString();
 		}
 	}
 }
