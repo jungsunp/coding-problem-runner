4b0cd4134954887ff975c82c6e8e572a8da18322
Jung Park <jungsun.park@relativity.com>
2024-12-15 15:19:42 -0600

    queue prob

--- src/CodingPractice/Leetcode/Queue.cs
+++ src/CodingPractice/Leetcode/Queue.cs
@@ -27,5 +27,33 @@ namespace CodingPractice.Leetcode
 				return this.queue.Count;
 			}
 		}
+
+		// #649. Dota2 Senate
+		// Time: O(n)
+		// Space: O(n)
+		public string PredictPartyVictory(string senate) {
+
+			// Build senatore queue
+			Queue<char> senQueue = new Queue<char>();
+			foreach (char c in senate) {
+				senQueue.Enqueue(c);
+			}
+
+			// Take turn and cast vote from the senator queue
+			Queue<char> voteQueue = new Queue<char>();
+			while(senQueue.Count > 0) {
+				if (voteQueue.Count < 1 || voteQueue.Peek() == senQueue.Peek()) {
+					// no senate to cast vote or vote from same party
+					voteQueue.Enqueue(senQueue.Dequeue());
+				}
+				else {
+					// vote from different party
+					senQueue.Dequeue(); // senator lose his rights
+					senQueue.Enqueue(voteQueue.Dequeue()); // senator who casted vote goes back to the queue
+				}
+			}
+
+			return voteQueue.Peek() == 'R' ? "Radiant" : "Dire";
+		}
 	}
 }
