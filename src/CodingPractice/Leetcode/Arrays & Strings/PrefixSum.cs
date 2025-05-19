2eef427a963f49aeac5eda42b6c7f48888b92985
Jung Park <jungsun.park@relativity.com>
2025-05-19 07:17:59 -0500

    another prefix sum prob

--- src/CodingPractice/Leetcode/Arrays & Strings/PrefixSum.cs
+++ src/CodingPractice/Leetcode/Arrays & Strings/PrefixSum.cs
@@ -42,4 +42,31 @@ namespace CodingPractice.Leetcode
 			return -1;
 		}
 	}
+
+	// 303. Range Sum Query - Immutable
+	public class NumArray {
+		private int[] sumArr;
+
+		// Time: O(n)
+		// Space: O(n)
+		public NumArray(int[] nums) {
+			this.sumArr = new int[nums.Length];
+
+			// compute sum from zero
+			int sum = 0;
+			for (int i = 0; i < nums.Length; i++) {
+				sum += nums[i];
+				this.sumArr[i] = sum;
+			}
+		}
+
+		// Time: O(1)
+		// Space: O(1)
+		public int SumRange(int left, int right) {
+			if (left == 0) {
+				return this.sumArr[right];
+			}
+			return this.sumArr[right] - this.sumArr[left - 1];
+		}
+	}
 }
