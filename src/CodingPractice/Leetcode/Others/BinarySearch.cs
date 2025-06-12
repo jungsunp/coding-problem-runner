9e4918da74bcffe7f5f5732372cfd85069a7bd63
Jung Park <jungsun.park@relativity.com>
2025-06-12 08:12:03 -0500

    binary serach prob

--- src/CodingPractice/Leetcode/Others/BinarySearch.cs
+++ src/CodingPractice/Leetcode/Others/BinarySearch.cs
@@ -46,14 +46,14 @@ namespace CodingPractice.Leetcode
 				int nextGuess = left + (right - left) / 2;
 				switch (guess(nextGuess))
 				{
-				case -1:
-				right = nextGuess - 1;
-				break;
-				case 1:
-				left = nextGuess + 1;
-				break;
-				default: // 0
-				return nextGuess;
+					case -1:
+						right = nextGuess - 1;
+						break;
+					case 1:
+						left = nextGuess + 1;
+						break;
+					default: // 0
+						return nextGuess;
 				}
 			}
 
@@ -275,6 +275,51 @@ namespace CodingPractice.Leetcode
 			}
 
 			return false; // not found
+		}
+
+		// #33. Search in Rotated Sorted Array
+		// Time: O(log n)
+		// Space: O(1)
+		public int Search(int[] nums, int target)
+		{
+			int left = 0;
+			int right = nums.Length - 1;
+
+			while (left <= right)
+			{
+				int mid = left + (right - left) / 2;
+
+				if (nums[mid] == target)
+				{
+					return mid;
+				}
+
+				if (nums[left] <= nums[mid])
+				{ // left half sorted
+					if (nums[left] <= target && target < nums[mid])
+					{
+						right = mid - 1;
+					}
+					else
+					{
+						left = mid + 1;
+					}
+
+				}
+				else
+				{ // right half sorted
+					if (nums[mid] < target && target <= nums[right])
+					{
+						left = mid + 1;
+					}
+					else
+					{
+						right = mid - 1;
+					}
+				}
+			}
+
+			return -1;
 		}
 	}
 }
