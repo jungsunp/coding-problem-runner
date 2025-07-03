e96ea541cc0b753be663b40b5e66bfd09396dd22
Jung Park <jungsun.park@relativity.com>
2025-07-03 12:14:42 -0500

    sparse vector using hash map

---
+++ src/CodingPractice/Leetcode/Hash Map/Custom/RandomizedSet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice.Leetcode.Hash_Map.Custom
+{
+	// #380. Insert Delete GetRandom O(1)
+	// Time: O(1) for all functions
+	// Space: O(n)
+	public class RandomizedSet
+	{
+		private Dictionary<int, int> hash; // map item to index in list
+		private List<int> list;
+		private Random random;
+
+		public RandomizedSet()
+		{
+			hash = new();
+			list = new();
+			random = new();
+		}
+
+		public bool Insert(int val)
+		{
+			if (hash.ContainsKey(val))
+			{
+				return false;
+			}
+
+			list.Add(val);
+			hash.Add(val, list.Count - 1);
+			return true;
+		}
+
+		public bool Remove(int val)
+		{
+			if (!hash.ContainsKey(val))
+			{
+				return false;
+			}
+
+			// Move the last element to index
+			int index = hash[val];
+			int lastIndex = list.Count - 1;
+			if (index < lastIndex)
+			{
+				int lastVal = list[lastIndex];
+				list[index] = lastVal;
+				hash[lastVal] = index;
+			}
+
+			// Remove with O(1) from both list and hash
+			list.RemoveAt(lastIndex);
+			hash.Remove(val);
+			return true;
+		}
+
+		public int GetRandom()
+		{
+			int rndNumber = random.Next(0, list.Count);
+			return list[rndNumber];
+		}
+	}
+}
---
+++ src/CodingPractice/Leetcode/Hash Map/Custom/SparseVector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice.Leetcode.Hash_Map.Custom
+{
+	public class SparseVector
+	{
+		// #1570. Dot Product of Two Sparse Vectors
+		// Time: O(n)
+		// Space: O(m) - m: number of non zero elements
+		public Dictionary<int, int> Hash
+		{
+			get; private set;
+		}
+		public SparseVector(int[] nums)
+		{
+			Hash = new();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] != 0)
+				{
+					Hash[i] = nums[i];
+				}
+			}
+		}
+
+		// Return the dotProduct of two sparse vectors
+		// Time: O(Min(m, l)) - l: number of non zero elements in vec
+		// Spce: O(1)
+		public int DotProduct(SparseVector vec)
+		{
+			var keys = Hash.Keys.Count < vec.Hash.Keys.Count ? Hash.Keys : vec.Hash.Keys;
+			int res = 0;
+			foreach (int key in keys)
+			{
+				if (Hash.ContainsKey(key) && vec.Hash.ContainsKey(key))
+				{
+					res += Hash[key] * vec.Hash[key];
+				}
+			}
+			return res;
+		}
+	}
+}
